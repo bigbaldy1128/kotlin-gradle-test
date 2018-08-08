be8961164d51ed98374dba9ffe0d220938fc635e
wangjinzhao <bigbaldy@163.com>
2018-08-08 19:44:08 +0800

    add coroutine

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,18 @@
-fun main(args: Array<String>) {
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.runBlocking
+
+fun main(args: Array<String>) = runBlocking {
     val start = System.currentTimeMillis()
-    val result = fab(45)
+    val result = listOf(calc(44), calc(43))
+            .map { p -> p.await() }
+            .sum()
     val end = System.currentTimeMillis()
     println("elapseTime:${end - start},result:$result")
+}
+
+fun calc(x: Int) = async(CommonPool) {
+    fab(x)
 }
 
 fun fab(num: Int): Int {
@@ -10,4 +20,10 @@ fun fab(num: Int): Int {
         return 1
     }
     return fab(num - 1) + fab(num - 2)
+}
+
+suspend fun calc2(x: Int): Int {
+    return async(CommonPool) {
+        fab(x)
+    }.await()
 }
